1d344f6175ab0d516bc5c8c88ffd0d62a3355784
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-21 18:07:01 +0000

    Add flexibility to the input of dates - using 'Chronic' gem

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,7 +4,7 @@ class TodoItem
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     add_priority options[:priority] if options.include? :priority
   end
   def details
