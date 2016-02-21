4e112f6fd5f0ce1de1cf286bd9a4099d9b1b4036
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-21 19:14:02 +0000

    Add ability to filter by item type

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -26,6 +26,16 @@ class UdaciList
       puts "#{position + 1}) #{item.details}"
     end
   end
+
+  def filter item_type
+    puts "-" * @title.length * 3
+    puts @title + " - Filtered By: #{item_type}"
+    puts "-" * @title.length * 3
+    @items.each_with_index do |item, position|
+      puts "#{position + 1}) #{item.details}" if item.class.to_s.downcase.include? item_type
+    end
+  end
+
   private
   def validItemType? type
     validItemTypes.include? type
